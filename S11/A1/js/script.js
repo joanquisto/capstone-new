287fa16446d727b6e49b527443a4d39908b05391
Joana Canoinigo <canonigo.joanaq@gmail.com>
2020-09-18 21:55:25 +0800

    added It's Sunday

---
+++ S11/A1/js/script.js
@@ -0,0 +1,19 @@
+let  Sunday = "Sunday";
+
+if(Sunday === Sunday){
+	console.log("It's Sunday");
+}else if(Monday === Monday ){
+	console.log("It's Monday");
+}else if(Sunday > Tuesay ){
+	console.log("It's not Sunday");
+}else if(Sunday > Wednesday ){
+	console.log("It's not Sunday");
+}else if(Sunday > Thursday ){
+	console.log("It's not Sunday");
+}else if(Sunday > Friday ){
+	console.log("It's not Sunday");
+}else if(Sunday > Saturday ){
+	console.log("It's not Sunday");
+}else{
+	console.log("Sorry, that is not a day in a week");
+}
---
+++ S11/D1/js/script.js
@@ -0,0 +1,114 @@
+// alert("Hello from JS");
+// alert("Hello from JS");
+// alert("Hello from JS");
+// alert("Hello from JS");
+// JavaScript syntax is the set of rules,
+ // how JavaScript programs are contucted.
+
+ // console.log("Hello World");
+ // console.log("Using JavaScript");
+
+// Variables - Variables are containers for data.
+
+
+
+
+// 
+// let fname = "Peter Griffin";
+// console.log(fname);
+
+// fname = "Homer Simpsons"
+// console.log(fname);
+
+// fname ='Ironman';
+// alert(fname);
+// console.log("Tony Stark");
+
+// let greeting;
+// greeting = "Hello! We're here!";
+// console.log(greeting);
+
+// greeting = '"She said Yes!"';
+// console.log(greeting);
+
+// let money = 200;
+// console.log(money);
+
+// vairabe and data types
+// string
+// purpose it will dectate what operation 
+// you can do to the variable
+
+// let name = "Peter Griffin";  //string
+//let num1 = 12;    			  //numeric/
+//let num2 = 3;                 //numeric
+//Concatination
+// console.log(name + " Family Guy");
+
+
+//strings length property
+// console.log(name.length); //13 w/c total of character
+// console.log(name[4]); //r from 0 -4 in peter
+
+//single or double qoutes OK
+//"hello world"
+//'hello world'
+
+//let name = "Peter Griffin";  //string
+//let num1 = 12;    			  //numeric
+//let num2 = 3;  //numeric
+//console.log(num1=num2) ;
+
+//let x = 2;
+//let y = 3;
+
+//console.log(x);
+//expected outpur:2
+
+////expecte output: 4
+//console.log(y = x * y); // 4*3
+//exprected output: 12
+
+//addition assignment operator
+//let bar = 5
+ 
+//number + number -> addition
+//bar += 2 //7
+
+//console.log(bar);
+
+//comparison operators
+//console.log(1 === 1);
+//expected output: true
+//console.log ("1" === "1");
+//expected output: false
+
+//console.log(1 == 1);
+//expected output: true
+//console.log ("1" == 1);
+//expected output: true reaso is it ignore datatype
+
+
+//console.log(1 != 2);
+///output : true
+///output should be:false
+
+//console.log(3 != '3');
+//true
+//console.log(4 != 3);
+///true
+
+
+
+//relationa operators
+let y = 4;
+let y = 3;
+
+console.log (x >= y);
+//true
+
+console.log (y >= x);
+//false
+
+console.log(x < y);
+//false
